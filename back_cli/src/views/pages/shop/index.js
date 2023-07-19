f6bd1209f8bb86a79d2df8623a5adae03718dc95
Vladimir Manzhos <manzhos@gmail.com>
2023-07-19 09:06:36 +0200

    clean stage

--- back_cli/src/views/pages/shop/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ShopListHead }    from './ShopListHead';
-export { default as ShopListToolbar } from './ShopListToolbar';
-export { default as ShopMoreMenu }    from './ShopMoreMenu';
